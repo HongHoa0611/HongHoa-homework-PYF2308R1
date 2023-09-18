12bbc72cc48b811379807a62be10c882e7d07df4
honghoa0611 <honghoa.0611@gmail.com>
2023-09-18 15:14:33 +0900

    bai_tap_W2

---
+++ HW-W2/displayInformation.py
@@ -0,0 +1,59 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "0eeb2721-89c0-482a-b51e-91c927410383",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "nhập tên của bạn: Hoa\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "tên tôi là Hoa\n"
+     ]
+    }
+   ],
+   "source": [
+    "name= input(\"nhập tên của bạn:\")\n",
+    "print (\"tên tôi là\", name)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "18b1624f-973e-4a59-8316-9e03a03ba0d7",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
